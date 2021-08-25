7b8dbe023cbda750d3934c8864bd1fe77af26538
bxokhxnhle <khanh.le@uwaterloo.ca>
2021-08-25 00:29:09 -0400

    added validation to the form

--- src/index.js
+++ src/index.js
@@ -9,37 +9,94 @@ class SignUpForm extends React.Component {
       firstName: '',
       lastName: '',
       email: '',
-      password: ''
-    };
+      password: '',
+      errors: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+      }
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
   }
-    handleChange(event, value) {
-      this.setState({[value]: event.target.value});
+
+  handleValidation(event) {
+    let name = event.target.name;
+    let value = event.target.value;
+    let errors = this.state.errors;
+    let formIsValid = true;
+
+    switch (name) {
+      case 'firstName':
+        if (value === '') {
+          errors.firstName = 'First Name cannot be empty';
+          formIsValid = false;
+        } else errors.firstName = '';
+        break;
+      case 'lastName':
+        if (value === '') {
+          errors.lastName = 'Last Name cannot be empty';
+          formIsValid = false;
+        } else errors.lastName = '';
+        break;
+      case 'email':
+        if (value === '') {
+          errors.email = 'Looks like this is not an email';
+          formIsValid = false;
+        } else errors.email = '';
+        break;
+      case 'password':
+        if (value === '') {
+          errors.password = 'Password cannot be empty';
+          formIsValid = false;
+        } else errors.firstName = '';
+        break;
+      default:
+        break;
     }
 
-    handleSubmit(event) {
-      alert('The form was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
+    this.setState({
+      [errors]: errors
+    });
 
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <input className="first-name" type="text" name="firstName" value={this.state.firstName} placeholder="First Name" onChange={(event)=> this.handleChange(event, "firstName")} />
-          <div class="error-last-name"></div>
+    return formIsValid;
+  }
+  
+  handleChange(event, value) {
+    this.setState({ 
+        [value]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
     
-          <input className="last-name" type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" onChange={(event)=> this.handleChange(event, "lastName")} />
-          
-          <input className="email" type="email" name="email" value={this.state.email} placeholder="Email" onChange={(event)=>this.handleChange(event, "email")} />
-          
-          <input className="password" type="password" name="password" value={this.state.password} placeholder="Password" onChange={(event)=>this.handleChange(event, "password")} />
-      
-          <input className="submit" type="submit" value="CLAIM YOUR FREE TRIAL" />
-        </form>
-      );
+    if (this.handleValidation(event)) {
+      alert('Valid Form: ' + this.state.value);
+    } 
+    else {
+      alert('Invalid Form: ' + this.state.value);
     }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input className="first-name" type="text" name="firstName" value={this.state.firstName} placeholder="First Name" onChange={(event)=> this.handleChange(event, "firstName")} />
+    
+        <input className="last-name" type="text" name="lastName" value={this.state.lastName} placeholder="Last Name" onChange={(event)=> this.handleChange(event, "lastName")} />
+
+        <input className="email" type="email" name="email" value={this.state.email} placeholder="Email" onChange={(event)=>this.handleChange(event, "email")} />
+
+        <input className="password" type="password" name="password" value={this.state.password} placeholder="Password" onChange={(event)=>this.handleChange(event, "password")} />     
+
+        <input className="submit" type="submit" value="CLAIM YOUR FREE TRIAL" />
+      </form>
+    );
+  }
 }
 
 ReactDOM.render(
